04afc47581832dc54f5fb444829c97cdb430eeb0
Evgensr <evgensr@gmail.com>
2022-02-15 16:13:07 +0300

    Increment 2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"io"
 	"log"
 	"net/http"
 )
 
 const (
-	port = "8080"
+	port     = "8080"
+	host     = "localhost"
+	protocol = "http"
+	limit    = 4096
 )
 
 var mapURL = make(map[string]string)
@@ -37,7 +42,7 @@ func handlerURL(w http.ResponseWriter, r *http.Request) {
 		mapURL[hash] = address
 		log.Printf("Address = %s\n", address)
 		w.WriteHeader(201)
-		w.Write([]byte("http://localhost:" + port + "/" + hash))
+		w.Write([]byte(protocol + "://" + host + ":" + port + "/" + hash))
 
 	case "GET":
 		urlHash := r.RequestURI[1:]
@@ -58,6 +63,62 @@ func handlerURL(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// handlerPOST — обработчик запроса.
+func HandlerPOST(w http.ResponseWriter, r *http.Request) {
+	log.Println("post request")
+	// читаем Body
+	b, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	log.Println(string(b))
+	address := string(b)
+
+	if len(address) > limit {
+		log.Println("big post request")
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	hash := GetHash(address)
+	log.Println(hash)
+	mapURL[hash] = address
+	log.Printf("Address = %s\n", address)
+	w.WriteHeader(201)
+	w.Write([]byte(protocol + "://" + host + ":" + port + "/" + hash))
+
+}
+
+// HandlerGET — обработчик запроса.
+func HandlerGET(w http.ResponseWriter, r *http.Request) {
+
+	urlHash := chi.URLParam(r, "hash")
+	log.Println(urlHash)
+	// len := len(urlHash)
+
+	if len(urlHash) != 32 {
+		w.WriteHeader(500)
+		return
+	}
+
+	// Вопрос, как еще можно проверить сущесовование map с таким ключом, без создания?
+	val, exists := mapURL[urlHash]
+	// log.Println(mapURL)
+	if exists {
+		log.Println(val)
+		w.Header().Set("Location", val)
+		w.WriteHeader(307)
+		w.Write(nil)
+	} else {
+		delete(mapURL, urlHash)
+		w.WriteHeader(400)
+	}
+
+}
+
 func GetHash(text string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(text))
@@ -68,11 +129,18 @@ func main() {
 
 	log.Println("start server")
 
-	// маршрутизация запросов обработчику
-	http.HandleFunc("/", handlerURL)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/{hash}", HandlerGET)
+	r.Post("/", HandlerPOST)
+
+	//// маршрутизация запросов обработчику
+	//http.HandleFunc("/", handlerURL)
 
 	// запуск сервера с адресом localhost, порт port
-	err := http.ListenAndServe(":"+port, nil)
+	// err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, r)
 
 	if err != nil {
 		log.Fatal(err)
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,7 +1,11 @@
 package main_test
 
 import (
+	"bytes"
 	main "github.com/evgensr/practicum1/cmd/shortener"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -15,9 +19,14 @@ func Test_getHash(t *testing.T) {
 		want string
 	}{
 		{
-			name: "pass md5",
+			name: "pass md5 of 1",
 			args: args{"1"},
 			want: "c4ca4238a0b923820dcc509a6f75849b",
+		},
+		{
+			name: "pass md5 of string example",
+			args: args{"example"},
+			want: "1a79a4d60de6718e8e5b326e338ae533",
 		},
 	}
 	for _, tt := range tests {
@@ -27,4 +36,73 @@ func Test_getHash(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_main(t *testing.T) {
+
+	// определяем структуру теста
+	type want struct {
+		code     int
+		request  string
+		response string
+	}
+	baseUrl := "http://localhost:8080/"
+
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code:     201,
+				request:  `https://ya.ru`,
+				response: baseUrl + "e98192e19505472476a49f10388428ab",
+			},
+		},
+		{
+			name: "positive test #2",
+			want: want{
+				code:     201,
+				request:  `https://habr.ru`,
+				response: baseUrl + "98981d87735b7f871c516eaf9b6bf461",
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+
+			reqBody := []byte(tt.want.request)
+			request := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(reqBody))
+
+			// определяем хендлер
+			h := http.HandlerFunc(main.HandlerPOST)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+		})
+	}
+
 }
